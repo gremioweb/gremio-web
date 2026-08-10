a5058a1354a624e6ba94873e7af2ce1257ce4e82
gremioweb <drprov11@gmail.com>
2026-08-11 01:12:41 +0200

    Add files via upload

--- netlify/functions/reviews.mjs
+++ netlify/functions/reviews.mjs
@@ -3,7 +3,10 @@ import { getStore } from "@netlify/blobs";
 function jsonResponse(data, status = 200) {
   return new Response(JSON.stringify(data), {
     status,
-    headers: { "content-type": "application/json; charset=utf-8" },
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "cache-control": "no-store, max-age=0",
+    },
   });
 }
 
